00e73fbc6930df4d6a050398fedc36ca998d7838
Marcos Adamczuk <marcos.adamczuk@gmail.com>
2017-04-19 09:35:30 -0300

    first commit

---
+++ avl.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _nodo{
+    int chave;
+    int altura, nivel;
+    int altdireita, altesquerda;
+    struct _nodo * esq;
+    struct _nodo * dir;
+    struct _nodo * pai;
+} TpNodo;
+
+typedef struct _arvore{
+    TpNodo * raiz;
+} TpArvore;
+
+TpArvore * inicializa();
+void imprime(TpArvore * arvore);
+TpArvore * insere(TpArvore * arvore, int chave);
+
+int main(){
+    TpArvore *arvore = (TpArvore*)malloc(sizeof(TpArvore));
+    arvore = inicializa();
+
+    int c;
+
+    while(scanf("%d", &c), c >= 0){
+        arvore = insere(arvore, c);
+        printf("vish....\n");
+        imprime(arvore);
+    }
+
+    return 0;
+}
+
+TpArvore * inicializa(){//aloca memoria para inicializar a arvore
+    TpArvore *arvore = (TpArvore*)malloc(sizeof(TpArvore));
+    arvore->raiz = NULL;
+    return arvore;
+}
+
+/*-----função auxiliar para a função insere----*/
+int max(int a, int b){
+    return a > b ? a : b;
+}
+
+/*-----função auxiliar para a função insere----*/
+TpNodo * _insere(TpNodo * pai, TpNodo * nodo){
+    nodo->pai = pai;
+    nodo->esq = nodo->dir = NULL;
+    nodo->altura = 0;
+
+    printf("oh god why\n");
+
+    TpNodo * aux;
+
+    if(pai == NULL) return nodo;
+
+    if(pai->dir == NULL && nodo->chave >= pai->chave){
+        //inserir nodo diretamente à direita do nodo "pai"
+        pai->dir = nodo;
+        nodo->altura = nodo->altesquerda = nodo->altdireita = 0;
+        nodo->nivel = pai->nivel + 1;
+        pai->altdireita = 1;
+        pai->altura = max(pai->altdireita, pai->altesquerda);
+    }
+    else if(pai->esq == NULL && nodo->chave <= pai->chave){
+        //inserir nodo diretamente à esquerda do nodo "pai"
+        pai->esq = nodo;
+        nodo->altura = nodo->altesquerda = nodo->altdireita = 0;
+        nodo->nivel = pai->nivel + 1;
+        pai->altesquerda = 1;
+        pai->altura = max(pai->altdireita, pai->altesquerda);
+    }
+    else if(pai->dir != NULL && nodo->chave >= pai->chave){
+        //inserir nodo no filho da direita do nodo "pai"
+        aux = _insere(pai->dir, nodo);
+        pai->dir = aux;
+        pai->altdireita = pai->dir->altura;
+        pai->altura = max(pai->altdireita, pai->altesquerda);
+    }
+    else if(pai->esq != NULL && nodo->chave <= pai->chave){
+        //inserir nodo no filho da esquerda do nodo "pai"
+        aux = _insere(pai->esq, nodo);
+        pai->esq = aux;
+        pai->altesquerda = pai->esq->altura;
+        pai->altura = max(pai->altdireita, pai->altesquerda);
+    }
+
+    return pai;
+}
+
+TpArvore * insere(TpArvore * arvore, int chave){
+    printf("wtf dude\n");
+    TpNodo * nodo = (TpNodo*)malloc(sizeof(TpNodo));
+    nodo->chave = chave;
+    arvore->raiz = _insere(arvore->raiz, nodo);
+    return arvore;
+}
+
+/*----função auxiliar para a função imprime-----*/
+void _imprime(TpNodo * nodo){
+    if(nodo == NULL) return;
+    for(int i = 0; i < nodo->nivel; i++)printf("    ");
+    printf("%d\n", nodo->chave);
+    _imprime(nodo->esq);
+    _imprime(nodo->dir);
+}
+
+void imprime(TpArvore * arvore){
+    _imprime(arvore->raiz);
+}
